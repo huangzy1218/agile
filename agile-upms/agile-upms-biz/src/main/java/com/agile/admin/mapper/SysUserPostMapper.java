09a2311e5252b72e31daa61199ba5a00650c8a94
huangzhengyang <uanghengyang@xiaomi.com>
2024-07-19 11:04:08 +0800

    feat: Add user related mappers

---
+++ agile-upms/agile-upms-biz/src/main/java/com/agile/admin/mapper/SysUserPostMapper.java
@@ -0,0 +1,12 @@
+package com.agile.admin.mapper;
+
+import com.agile.admin.api.entity.SysUserPost;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Huang Z.Y.
+ */
+@Mapper
+public interface SysUserPostMapper extends BaseMapper<SysUserPost> {
+}
